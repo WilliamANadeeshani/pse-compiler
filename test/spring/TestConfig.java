8eb5e91c61427efa97f0ca4a7ba5d58dedceebb8
WilliamANadeeshani <nadeeshaniwilliam@gmail.com>
2023-02-18 13:11:38 +0100

    added spring DI testing

--- test/spring/TestConfig.java
+++ test/spring/TestConfig.java
@@ -1,18 +1,18 @@
 package spring;
 
-import mockito.Mockito;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-//
-//import static org.mockito.Mockito.mock;
-//
-//public class TestConfig {
-//    @Bean
-//    public InterfaceA A() {
-//        return mock(InterfaceA.class);
-//    }
-//
-//    @Bean B B() {
-//        return mock(B.class);
-//    }
-//}
+
+import static org.mockito.Mockito.mock;
+@ComponentScan("spring")
+public class TestConfig {
+    @Bean
+    public InterfaceA A() {
+        return mock(InterfaceA.class);
+    }
+
+    @Bean B B() {
+        return mock(B.class);
+    }
+}
--- test/spring/springTest.java
+++ test/spring/springTest.java
@@ -1,11 +1,33 @@
 package spring;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class springTest {
 
+    private ApplicationContext context;
+    private Controller controller;
+
+    private InterfaceA a;
+
+    @BeforeEach
+    void setUp() {
+        context = new AnnotationConfigApplicationContext(TestConfig.class);
+        controller = context.getBean(Controller.class);
+        a = context.getBean(InterfaceA.class);
+    }
+
     @Test
     public void testDI() {
-
+        when(a.getValue()).thenReturn(12);
+        int actual = controller.getValue(100);
+        assertEquals(112, actual);
+        verify(a).getValue();
     }
 }
